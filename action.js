feeda4a1ebe9bb9a031351f4745a29a84431375b
PatriziaCrimi <crimi.patrizia@gmail.com>
2020-11-09 15:53:47 +0100

    Basic files HTML, style with CSS & JS uploaded

---
+++ action.js
@@ -0,0 +1,16 @@
+/*ASSIGNMENT
+Rifate lo slider di immagini con Vue.
+Aggiungete anche i pallini che si "muovono" di pari passo con le immagini.
+BONUS 1: l'autoplay con una timing functions.
+Piccolo consiglio: cercate nella documentazione di vue come fare per eseguire una funzione quando l'istanza di Vue è stata creata.
+BONUS 2: cliccando su un pallino, si attiva l'immagine corrispondente.
+*/
+
+let app = new Vue({
+  el: 'root',
+  data: {
+
+  },
+  methods: {
+  },
+});
